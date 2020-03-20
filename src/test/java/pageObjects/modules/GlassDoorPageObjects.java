425bc9819c11737536cfd78c28e0388f62a33059
Vivek <vivekautomationconsultant@gmail.com>
2020-03-20 12:46:10 +0530

    added additional files

---
+++ src/test/java/pageObjects/modules/GlassDoorPageObjects.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pageObjects.modules;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import pageObjects.initializePageObjects.PageFactoryInitializer;
+import utils.RandomGenerator;
+import utils.ExtentReports.ExtentTestManager;
+import com.relevantcodes.extentreports.LogStatus;
+
+/**
+ * @Author Vivekanandan Sampath
+ * @Date Mar 09, 2020
+ */
+public class GlassDoorPageObjects extends PageFactoryInitializer {
+	
+
+}
