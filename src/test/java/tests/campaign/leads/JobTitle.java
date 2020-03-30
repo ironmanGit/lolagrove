9ce6e63efa3c333fe93e59b4bcd92041d834edef
Vivek <vivekautomationconsultant@gmail.com>
2020-03-30 13:26:31 +0530

    added upto job title test

---
+++ src/test/java/tests/campaign/leads/JobTitle.java
@@ -0,0 +1,39 @@
+package tests.campaign.leads;
+
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import pageObjects.modules.LeadPageObjects;
+import utils.ExtentReports.ExtentTestManager;
+
+public class JobTitle extends LeadPageObjects {
+
+	private Logger logger = Logger.getLogger(LeadPageObjects.class.getName());
+
+	public LeadPageObjects jobTitleCheck() throws Exception {
+		String openNotesJobTitle = campaignTestDataProcess().getLeadsJobTitle();
+		try {
+			ExtentTestManager.getTest().log(LogStatus.INFO, "job title check in linkedin");
+			selectLinkedInCompanySerachInJobTitleDropdown();
+			String jobTitle = linkedInPage().getJobTitleValue();
+			if(openNotesJobTitle.contains(jobTitle)) {
+				ExtentTestManager.getTest().log(LogStatus.PASS, "Job title in lead page matched with linkedin job title");
+			} else {
+				setvalueJobTitle(jobTitle);
+				ExtentTestManager.getTest().log(LogStatus.PASS, "Job title in lead page is updated with linkedin job title");
+			}
+			String linkedInUrl = getCurrentUrl();
+			linkedInPage().closeLinkedInPage();
+			setvalueJobTitleEvidence(linkedInUrl);
+			ExtentTestManager.getTest().log(LogStatus.INFO,"job title url evidence is updated in lead page");
+		} catch (Exception e) {
+			setvalueJobTitleEvidence("Job title evidence not found");
+			ExtentTestManager.getTest().log(LogStatus.INFO,"Job title evidence not found");
+		}
+		return leadPage();
+	}
+
+}
+
